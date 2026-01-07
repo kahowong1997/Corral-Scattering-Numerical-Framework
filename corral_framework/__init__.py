8609aa988713db6a020e59f251b602f60155f5d9
Ka Ho Wong <kwong49@uic.edu>
2026-01-07 14:33:31 -0600

    Create __init__.py

---
+++ corral_framework/__init__.py
@@ -0,0 +1,23 @@
+"""
+Corral Framework
+----------------
+A numerical simulation framework for computing the renormalized Green's function 
+and effective topological Hamiltonians for Magnetic-Superconductor Hybrid system 
+confined in a quantum corral, with either quasi-1D or arbitrary 2D geometry.
+"""
+
+from .MSH_Quasi_1D import Topo_Ham_1D
+from .MSH_2D_Library import (
+    build_GF_library, build_GF_matrix_final, build_corral_basis, build_T_matrix, Topo_Ham_2D
+)
+
+__version__ = "0.1.0"
+
+__all__ = [
+    "Topo_Ham_1D",
+    "build_GF_library",
+    "build_GF_matrix_final",
+    "build_corral_basis",
+    "build_T_matrix",
+    "Topo_Ham_2D"
+]
